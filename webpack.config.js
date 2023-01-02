c7bc9b72bda29490eecc77b570aa9ca65dfddee8
Tetiana Siestova <siestova.t@gmail.com>
2023-01-02 13:21:44 +0200

    project added

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const isProduction = process.env.NODE_ENV == "production";
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const stylesHandler = "style-loader";
 const CopyPlugin = require("copy-webpack-plugin");
